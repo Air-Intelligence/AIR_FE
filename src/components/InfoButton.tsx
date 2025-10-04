8d80d87704a19e8606d5f416081fa1c7d8580438
kimyounghee425 <doctorsean0425@gmail.com>
2025-10-05 01:19:57 +0900

    [fix] 인포버튼 색상 변경

--- src/components/InfoButton.tsx
+++ src/components/InfoButton.tsx
@@ -12,7 +12,7 @@ export const InfoButton = ({ onResetOnboarding }: InfoButtonProps) => {
                     localStorage.removeItem("hasSeenOnboarding");
                     onResetOnboarding();
                 }}
-                className="w-16 h-16 bg-[#ff7f00] rounded-2xl flex items-center justify-center shadow-md"
+                className="w-16 h-16 bg-[#FFCE48] rounded-2xl flex items-center justify-center shadow-md"
             >
                 <InfoIcon className="w-8 h-8 text-black" />
             </button>
